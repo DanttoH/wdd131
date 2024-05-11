b12d3241d25f7a822c992afbbd6311bd971f88ff
DanttoH <her23001@byui.edu>
2024-05-11 20:11:35 -0300

    updated

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+
+document.getElementById("currentYear").textContent = myYear + "💪🏻Dantto Hermosilla✌🏻 Mendoza, Argentina";
+
+document.getElementById("lastModified").textContent = "Last Modification: " +
+    document.lastModified;
